2b5055a2dad00944c22a0a2110dfbc656c020d8b
Benn Lockyer <benn.lockyer@hotmail.com>
2015-04-18 16:54:22 +1000

    Big Update!
    The pet is now functional!
    He eats food and enemies. Keeps running when eating enemies and only
    keeps going after food if there's more.
    Environment layer set up for collision raycasting.
    Some basic animations in for Idle and Eating (idle is the same frame on
    repeat lol)
    Jump triggers have a class which we can use to define jumping
    restrictions (none - jump both directions. left/right - only jump when
    running that way)

---
+++ UnityProjectFolder/LudumDare32/Assets/Scripts/JumpTrigger.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpTrigger : MonoBehaviour 
+{
+	public enum DirectionLimit {None,Left,Right};
+	public DirectionLimit directionLimit;
+}

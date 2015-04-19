9ac277d46dd54fcc0e2fd3176e6ae85c95a2544b
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-04-19 12:03:04 -0400

    DemoBuild?
    I suppose this is the demo build for our game. I might update with some
    pause feature.

---
+++ UnityProjectFolder/LudumDare32/Assets/PauseManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseManager : MonoBehaviour {
+
+	//Will input pause later, for now, just Quit
+
+
+	void Update(){
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+
+			Application.Quit();
+		}
+	}
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+}

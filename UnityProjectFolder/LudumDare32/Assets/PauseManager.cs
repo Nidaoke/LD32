c86022c34be90b789467c306046ff863475cb645
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-04-19 12:37:07 -0400

    New Build with Pause shtuffz
    I put in a pause manager, currently only on level one and menu since
    those are the only playable ones. Pressing escape will immediately quit
    the game, and pressing P will allow you to "Continue", "Quit", or "Main
    Menu". I just put them in there so they're not very pretty :panda_face:

--- UnityProjectFolder/LudumDare32/Assets/PauseManager.cs
+++ UnityProjectFolder/LudumDare32/Assets/PauseManager.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class PauseManager : MonoBehaviour {
 
-	//Will input pause later, for now, just Quit
-
+	public bool isPaused = false;
 
 	void Update(){
 
@@ -12,9 +11,44 @@ public class PauseManager : MonoBehaviour {
 
 			Application.Quit();
 		}
+
+		if (isPaused) {
+
+			Time.timeScale = 0f;
+		} else {
+
+			Time.timeScale = 1f;
+		}
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+
+			if(Application.loadedLevel != 0){
+
+				isPaused = !isPaused;
+			}
+		}
 	}
 
-	void Awake() {
-		DontDestroyOnLoad(transform.gameObject);
+	void OnGUI() {
+
+		if (isPaused) {
+
+			if (GUI.Button (new Rect ((Screen.width / 2) - (Screen.width / 20), (Screen.height / 3), Screen.width / 10, Screen.height / 10), "Continue")) {
+				
+				isPaused = false;
+			}
+			if (GUI.Button (new Rect ((Screen.width / 2) - (Screen.width / 20), (Screen.height / 2) - (Screen.height / 20), Screen.width / 10, Screen.height / 10), "Quit")) {
+				
+				isPaused = false;
+				Application.Quit();
+			}
+			if (GUI.Button (new Rect ((Screen.width / 2) - (Screen.width / 20), (Screen.height / 1.6f) - (Screen.height / 20), Screen.width / 10, Screen.height / 10), "Main Menu")) {
+				
+				isPaused = false;
+				Application.LoadLevel(0);
+				Destroy(gameObject);
+			}
+		}
+		
 	}
 }

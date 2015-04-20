05cd52914e68d735801359fe45dc7e7d16f31299
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-04-20 14:17:28 -0400

    Added Camera Shake

---
+++ UnityProjectFolder/LudumDare32/Assets/CameraShakeLD.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShakeLD : MonoBehaviour {
+
+	float strength;
+	float mShakeTime = 0f;
+
+	Vector3 mStartingPosition;
+
+	void Start(){
+
+		mStartingPosition = transform.position;
+	}
+
+	void Update(){
+
+		if (mShakeTime > 0) {
+
+			tsransform.position = mStartingPosition + (Random.insideUnitSphere * strength);
+
+			mShakeTime -= Time.deltaTime;
+		} else {
+
+			mShakeTime = 0f;
+
+			transform.position = mStartingPosition;
+		}
+	}
+
+	public void ShakeCamera(float voidStrength, float voidDuration){
+
+		strength = voidStrength;
+		mShakeTime = voidDuration;
+	}
+}

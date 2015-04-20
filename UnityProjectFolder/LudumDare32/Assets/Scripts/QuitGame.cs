5570951226a889080a05fed6b668231526288f81
ADAM-M <adamamatheny@gmail.com>
2015-04-20 17:00:47 -0400

    Quit Game Buttton added
    Added a "Quit" button to both the pause menu and the main menu

---
+++ UnityProjectFolder/LudumDare32/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour 
+{
+
+	//Quits the game (meant to be attached to a button) ~Adam
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
